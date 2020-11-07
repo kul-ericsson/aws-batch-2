b2bfcb6d89a6d6453dba2e7531daf91b4c6310c7
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-07 15:47:24 +0530

    added ROUTE TABLE

--- vpc_resource.tf
+++ vpc_resource.tf
@@ -32,3 +32,14 @@ resource "aws_internet_gateway" "kul-gateway"{
 		Name = var.tagname
 	}
 }
+
+resource "aws_route_table" "kul-rt"{
+	vpc_id = aws_vpc.kul-vpc.id
+	route {
+		cidr_block = "0.0.0.0/0"
+		gateway_id = aws_internet_gateway.kul-gateway.id
+	}
+	tags = {
+		Name = var.tagname
+	}
+}
