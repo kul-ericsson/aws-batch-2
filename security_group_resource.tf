8b08a153d874faf89a32debfdfbe63953534cdca
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-07 11:12:14 +0530

    Creating Security Group with All Outbound open and Only SSH open for Inbound

---
+++ security_group_resource.tf
@@ -0,0 +1,16 @@
+resource "aws_security_group" "sg_1"{
+	name = "kul-sg"
+	description = "Created from Terraform by Kul"
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
