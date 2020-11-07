b86770b4fb98e59c8a74391e93c5c139d65ec506
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-07 16:40:59 +0530

    added MYSQL provider

--- providers.tf
+++ providers.tf
@@ -3,3 +3,9 @@
 provider "aws" {
 	region = var.aws_region
 }
+
+provider "mysql" {
+	endpoint = aws_db_instance.ericsson_mysql.endpoint
+	username = aws_db_instance.ericsson_mysql.username
+	password = aws_db_instance.ericsson_mysql.password
+}
