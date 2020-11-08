2a32a2e815857e8d20f6ed2042f418b4be4f5954
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-08 19:29:16 +0530

    updated to print mysql endpoint

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "server_public_ip" {
 	value = aws_instance.server_1.*.public_ip
 }
-output "server_public_dns" {
-	value = aws_instance.server_1.*.public_dns
+output "ericsson_MYSQL_ENDPOINT" {
+	value = aws_db_instance.ericsson_mysql.endpoint
 }
