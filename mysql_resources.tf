a5126ce8aacbb5320b0ac5b705e40fbe43ec76ff
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-08 19:30:55 +0530

    updated to grant permission to user

--- mysql_resources.tf
+++ mysql_resources.tf
@@ -5,4 +5,12 @@ resource "mysql_database" "kul"{
 resource "mysql_user" "dbuser" {
 	user = "kmayer"
 	plaintext_password = "india@123"
+	host = aws_instance.server_1[0].public_ip
+}
+
+resource "mysql_grant" "dbgrant" {
+	user = mysql_user.dbuser.user
+	host = mysql_user.dbuser.host
+	database = mysql_database.kul.name
+	privileges = ["SELECT","UPDATE","INSERT","CREATE","DROP","DELETE"]
 }
