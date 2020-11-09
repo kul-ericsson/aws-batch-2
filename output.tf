1dd849cdcf074b58af21501d68a8399b939c2cd6
Kulbhushan <kulbhushan.mayer@thinknyx.com>
2020-11-09 10:31:45 +0530

    print public IP of server already running in AWS fetched from Data Resource in AWS

--- output.tf
+++ output.tf
@@ -4,3 +4,7 @@ output "server_public_ip" {
 output "ericsson_MYSQL_ENDPOINT" {
 	value = aws_db_instance.ericsson_mysql.endpoint
 }
+
+output "running_server_public_ip" {
+	value = data.aws_instance.running_server.public_ip
+}
